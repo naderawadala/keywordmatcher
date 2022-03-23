607b8b59aeabc10ea60ff8fcefad1c122f1578fb
Nader Awadala <naderawadala@gmail.com>
2022-03-23 15:25:36 +0200

    minor update

--- lib/keywordmatcher.ex
+++ lib/keywordmatcher.ex
@@ -9,23 +9,43 @@ defmodule Keywordmatcher do
 
   def match?(text, keyword) do
 
-    text = " has a really cool and robust functional concurrent as well as possessing the capability of working on BEAM which is not
-     a programming language however Erlang allows us to be build cool applications and build distributed stuff, also bla bla bla there is this thing
-     called HEX DOC and then there's unit testing with uhh ExUnit or whatever, then there's also this cool thing called the Phoenix Framework"
+    #text = " has a really cool and robust functional concurrent as well as possessing the capability of working on BEAM which is not
+     #a programming language however Erlang allows us to be build cool applications and build distributed stuff, also bla bla bla there is this thing
+     #called HEX DOC and then there's unit testing with uhh ExUnit or whatever, then there's also this cool thing called the Phoenix Framework"
 
-    keyword = "(Elixir) OR (functional concurrent) OR BEAM OR (programming language) OR (Erlang application*) OR (build* distributed) OR (HEX DOC) OR ExUnit"
+    #keyword = "(Elixir) OR (functional concurrent) OR BEAM OR (programming language) OR (Erlang application*) OR (build* distributed) OR (HEX DOC) OR ExUnit"
+    #keyword = "has a really* cool"
+
+    conditions = List.insert_at([],0,has_OR(keyword))
+    |> List.insert_at(1,has_AND(keyword))
+    |> List.insert_at(2,has_wildcard(keyword))
+
+    case conditions do
+      [false,true, _] -> get_match(text,keyword)
+      [true, _, _] -> operation_OR?(text,keyword)
+      [false, false, true] -> Regex.match?(~r/#{keyword}/i, text)
+      [false,false,false] -> get_match(text,keyword)
+    end
 
 
-
-    kw_list = split_kw(keyword)
-    match_list(text,kw_list)
-    #|> Enum.member?(true)
 
   end
 
+  def operation_OR?(text,keyword) do
+    kw_list = split_kw_OR(keyword)
+    match_list(text,kw_list)
+    |> Enum.member?(true)
+
+  end
+
+  #get direct match for each word
   defp get_match(text,keyword) do
     if(has_wildcard(keyword)) do
-
+      String.replace(keyword,"*",".*")
+      case Regex.match?(~r/#{keyword}/i, text) do
+        true -> true
+        false -> false
+      end
     else
     case Regex.match?(~r/\W*((?i)#{keyword}(?-i))\W*/, text) do
       true -> true
@@ -34,6 +54,7 @@ defmodule Keywordmatcher do
   end
 end
 
+# remove parantheses from text, iterate through the list of keywords and get a match
   defp match_list(text,kw_list) do
     split_text = String.split(text,~r{\(})
     |> Enum.join()
@@ -45,7 +66,8 @@ end
     end
   end
 
-  def split_kw(keyword) do
+  #split keyword string into a list of elements seperated by the OR operator
+  def split_kw_OR(keyword) do
     String.split(keyword, ~r{\sOR\s})
   end
 
--- test/keywordmatcher_test.exs
+++ test/keywordmatcher_test.exs
@@ -1,78 +1,169 @@
 defmodule KeywordmatcherTest do
   use ExUnit.Case
   @module Keywordmatcher
-
-  describe "match?" do
-    test "return true if kw is in" do
-      keyword = "one"
-      text = "one OR three"
-
-      actual = @module.match?(text,keyword)
-      assert actual == true
-    end
-
-    test "test sentence of ands" do
-      keyword = "one two three four"
-      text = "one two three four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
-
-    test "test case insensitivity" do
-      keyword = "One Two Three FOUR"
-      text = "one two three four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
-
-    test "test parantheses" do
-      keyword = "(one) two three four"
-      text = "one two (three) four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
-
-    test "test sentence of ORS" do
-      keyword = "one OR RO ninetyneneen ooo bugsbunny randomtext for no reason whatsoever"
-      text = "one two three four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
-
-    test "order of words does not matter with OR operator" do
-      keyword = "five OR two one three"
-      text = "one two three four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
-
-    test "returns false if there are no matches at all" do
-      keyword = "five two OR one three"
-      text = "sixtynine fifty four thirty fivftenteoon"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == false
-    end
-
-    test "match wildcard" do
-      keyword = "five* three"
-      text = "one two three four five six seven"
-
-      actual = @module.match?(text,keyword)
-
-      assert actual == true
-    end
+  setup [:data]
+  defp data(context) do
+    text = "Elixir is a functional, concurrent, general-purpose programming language that runs on the BEAM virtual machine which is also used to implement the Erlang programming language. Elixir builds on top of Erlang and shares the same abstractions for building distributed, fault-tolerant applications. lang:de"
+    Map.put(context, :text, text)
   end
 
+  describe "match?" do
+    test "should return true if keyword appears in text", context do
+      keyword = "elixir"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should return true if keyword is written in uppercase", context do
+      keyword = "ELIXIR"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should return true if keyword is written in mixed case", context do
+      keyword = "Elixir"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should return true if keyword is surrounded with parantheses", context do
+      keyword = "(elixir)"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should return false if keyword does not appear in text", context do
+      keyword = "java"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    # WORD BOUNDARIES
+    test "WILDCARD | should return true if keyword appears as complete word", context do
+      keyword = "elixir*"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "WILDCARD | should return true if keyword appears as substring", context do
+      keyword = "function*"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "WILDCARD | should return false if keyword does not appear at all", context do
+      keyword = "foo*"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    test "NO WILDCARD | should return true if keyword appears as complete word", context do
+      keyword = "elixir"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "NO WILDCARD | should return false if keyword appears as substring", context do
+      keyword = "function"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    test "NO WILDCARD | should return false if keyword does not appear at all", context do
+      keyword = "foo"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    # MULTIPLE KEYWORDS
+    test "AND | should return true if all keywords appear in text", context do
+      keyword = "language elixir erlang"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "AND | should return false if one keyword is missing in text", context do
+      keyword = "language ruby erlang"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    test "AND | should return false if all keywords are missing in text", context do
+      keyword = "java ruby jvm"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    test "AND | should not matter if parantheses were set", context do
+      keyword = "(language elixir (erlang))"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "OR | should return true if all keywords appear in text", context do
+      keyword = "language OR elixir OR erlang"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "OR | should return true if one keyword is missing in text", context do
+      keyword = "language OR ruby OR erlang"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "OR | should return false if all keywords are missing in text", context do
+      keyword = "java OR ruby OR jvm"
+      assert @module.match?(context.text, keyword) == false
+    end
+
+    test "OR | should not matter if parantheses were set", context do
+      keyword = "((language) OR elixir OR (erlang))"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    # SPECIAL CASES
+    test "should not matter if opening and closing parantheses do not match", context do
+      keyword = "(elixir OR (ruby"
+      assert @module.match?(context.text, keyword) == true
+      keyword = "(elixir) OR ruby)"
+      assert @module.match?(context.text, keyword) == true
+      keyword = "())elixir OR ruby("
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should not matter if the 'OR' operator is written in downcase", context do
+      keyword = "elixir or ruby"
+      assert @module.match?(context.text, keyword) == true
+      keyword = "elixir Or ruby"
+      assert @module.match?(context.text, keyword) == true
+      keyword = "elixir oR ruby"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    test "should not matter how much whitespace is surrounding the 'OR' operator", context do
+      keyword = "elixir  OR  ruby"
+      assert @module.match?(context.text, keyword) == true
+      keyword = "elixir     OR     ruby"
+      assert @module.match?(context.text, keyword) == true
+      # Intentionally inserted tab characters here
+      keyword = "elixir    OR  ruby"
+      assert @module.match?(context.text, keyword) == true
+    end
+
+    # COMPLEX EXAMPLES
+    test "should work corretly for real world example 01", _context do
+      keyword =
+        "(Ölmühle OR (Ölz bäcker*) OR ÖNB OR Örag OR ÖSA OR Öschberghof OR ÖSEG OR Österreich OR (Österreich Werbung) OR österreich*) AND (lang:de OR lang:en OR lang:fr OR lang:it)"
+
+      assert @module.match?("lorem Österreich ipsum", keyword) == true
+      assert @module.match?("lorem Werbung ipsum", keyword) == false
+      assert @module.match?("lorem Ölz bäckerei ipsum", keyword) == true
+    end
+
+    test "should work corretly for real world example 02", _context do
+      keyword =
+        "(ÖHTB OR ÖKOFEN OR (ÖKON* SERV) OR (Ökosozialen Forum) OR (Ökosoziales Forum) OR (Ökumenisch* Gemeinschaftswerk Pfalz) OR Öl) AND (lang:de OR lang:en OR lang:fr OR lang:it)"
+
+      assert @module.match?("lorem ÖKOFEN ipsum", keyword) == true
+      assert @module.match?("lorem ökumenisches ipsum Gemeinschaftswerk", keyword) == false
+      assert @module.match?("lorem ökumenisches ipsum Pfalz", keyword) == false
+
+      assert @module.match?("lorem ökumenisches ipsum Gemeinschaftswerk dolor Pfalz", keyword) ==
+               true
+    end
+
+    test "should work corretly for real world example 03", _context do
+      keyword =
+        "(ÖVP OR ÖWD OR (ÖWG Wohnbau*) OR (Özlem Ünsal) OR Ørsted OR (Überkinger wasser*) OR (Überlandwerk Groß-Gerau)) AND (lang:de OR lang:en OR lang:fr OR lang:it)"
+
+      assert @module.match?("lorem Ørsted ipsum", keyword) == true
+      assert @module.match?("lorem Wasserwerk ipsum", keyword) == false
+      assert @module.match?("lorem Überkinger ipsum", keyword) == false
+      assert @module.match?("lorem Wasserwerk ipsum Überkinger", keyword) == true
+    end
+  end
 end
